1384773dd2f8d052c5f966e3643fafdc974151ca
adexp01 <andreydexp01@gmail.com>
2026-07-02 17:30:39 +0200

    fix

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const CRM_ORIGIN = "https://crm-ukr-consul.vercel.app";
+export const CRM_ORIGIN = "https://crm-urkconsul-api-production.up.railway.app";
 export const NEWS_API_URL = `${CRM_ORIGIN}/api/news`;
 
 export const getNewsApiUrl = () => NEWS_API_URL;
